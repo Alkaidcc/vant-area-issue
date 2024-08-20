5268417c842c677b5386c9869a793c9d1b6483b1
Alkaidcc <alkaidcc@qq.com>
2024-08-20 19:51:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+const formModel = ref({})
+onMounted(() => {
+  // mock data fetching
+  setTimeout(() => {
+    formModel.value = {
+      addrCode: '360203'
+    }
+  }, 500)
+})
+</script>
+
+<template>
+  <div id="app">
+    <HelloWorld v-model="formModel" />
+  </div>
+</template>
+
+<style>
+body {
+  font-size: 16px;
+  background-color: #f8f8f8;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <van-field :model-value="formModel.addrName" name="addrName" label="目的地" is-link readonly placeholder="请选择地区"
+      @click="showAreaPicker = true" />
+    <van-popup v-model:show="showAreaPicker" :lazy-render="false" round position="bottom">
+      <van-area ref="areaRef" :model-value="formModel.addrCode" title="目的地" :area-list="areaList" />
+    </van-popup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { areaList } from '@vant/area-data'
+import { ref, watch } from 'vue'
+const formModel = defineModel<any>()
+const props = defineProps({
+  code: {
+    type: String
+  }
+})
+const showAreaPicker = ref(false)
+const areaRef = ref()
+watch(formModel, (val) => {
+  console.log('val',val)
+  console.log('options', areaRef.value.getSelectedOptions())
+})
+</script>
+
+<style lang="less" scoped></style>
